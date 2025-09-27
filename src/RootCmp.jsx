f5947b404fdd191302483bd0dbaf017179ef4423
AndreiYakerson <tubikwheels@gmail.com>
2025-09-27 12:41:54 +0300

    Refactor RootCmp

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { Routes, Route } from 'react-router'
 
 import { HomePage } from './pages/HomePage'
-import { AboutUs, AboutTeam, AboutVision } from './pages/AboutUs'
-import { CarIndex } from './pages/CarIndex.jsx'
-import { ReviewIndex } from './pages/ReviewIndex.jsx'
+import { AboutUs } from './pages/AboutUs.jsx'
+import { BoardIndex } from './pages/BoardIndex.jsx'
 import { ChatApp } from './pages/Chat.jsx'
-import { AdminIndex } from './pages/AdminIndex.jsx'
 
-import { CarDetails } from './pages/CarDetails'
+
+import { BoardDetails } from './pages/BoardDetails.jsx'
 import { UserDetails } from './pages/UserDetails'
 
 import { AppHeader } from './cmps/AppHeader'
@@ -26,16 +25,11 @@ export function RootCmp() {
             <main>
                 <Routes>
                     <Route path="" element={<HomePage />} />
-                    <Route path="about" element={<AboutUs />}>
-                        <Route path="team" element={<AboutTeam />} />
-                        <Route path="vision" element={<AboutVision />} />
-                    </Route>
-                    <Route path="car" element={<CarIndex />} />
-                    <Route path="car/:carId" element={<CarDetails />} />
+                    <Route path="about" element={<AboutUs />} />
+                    <Route path="board" element={<BoardIndex />} />
+                    <Route path="board/:boardId" element={<BoardDetails />} />
                     <Route path="user/:id" element={<UserDetails />} />
-                    <Route path="review" element={<ReviewIndex />} />
                     <Route path="chat" element={<ChatApp />} />
-                    <Route path="admin" element={<AdminIndex />} />
                     <Route path="auth" element={<LoginSignup />}>
                         <Route path="login" element={<Login />} />
                         <Route path="signup" element={<Signup />} />
