1dfd56228b2d9f55f68cfdade21f23cb0acf3fbe
AndreiYakerson <tubikwheels@gmail.com>
2025-09-27 23:28:42 +0300

    Create TaskDetails cmp

---
+++ src/cmps/TaskDetails.jsx
@@ -0,0 +1,11 @@
+import { DynamicCmp } from "./DynamicCmp";
+
+export function TaskDetails({ task }) {
+    return (
+        <div className="task-details">
+            {
+                //TODO <DynamicCmp />
+            }
+        </div>
+    )
+}
