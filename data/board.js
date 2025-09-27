7c1290024f30d5eb326f195da30c2d67045f77ad
eliad <eliad.haim@gmail.com>
2025-09-27 21:16:32 +0300

    Add data folder

---
+++ data/board.js
@@ -0,0 +1,20 @@
+const board = {
+    _id: 'g101',
+    title: 'board',
+    createdAt: 1589983468418,
+    groups: [
+        {
+            id: 'g101',
+            title: 'Group',
+            createdAt: 1589983468418,
+            tasks: [
+                {
+                    id: 'c101',
+                    title: 'task',
+                    createdAt: 1589983468418,
+                },
+            ],
+        },
+    ],
+}
+
