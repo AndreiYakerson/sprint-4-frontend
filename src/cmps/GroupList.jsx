34696fb978d1530d3a25c8374d06b3f15c66318c
Your Name <dan.weibren@gmail.com>
2025-09-28 16:08:11 +0300

    - Bordered the group containers.

--- src/cmps/GroupList.jsx
+++ src/cmps/GroupList.jsx
@@ -1,14 +1,14 @@
 import { TaskList } from "./Task/TaskList"
 
 export function GroupList({ groups }) {
-    console.log("🚀 ~ GroupList ~ groups:", groups)
     return (
         <section className="group-list">
             {groups.map(group => {
-                return <div key={group.id} className="group-container">
+                return <div key={group.id}>
                     <header>{group.title}</header>
-                    
-                    <TaskList tasks={group.tasks} />
+                    <section className="group-container">
+                        <TaskList tasks={group.tasks} />
+                    </section>
                 </div>
             })}
         </section>
