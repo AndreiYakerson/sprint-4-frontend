01ad87a8daeca39c5581bb09d54a568b9a6061df
Your Name <dan.weibren@gmail.com>
2025-09-28 16:38:40 +0300

    - Replace TaskDetails. With TaskPreview inside TaskList.
    Applied it without the dynamic. For now.

--- src/cmps/Task/TaskPreview.jsx
+++ src/cmps/Task/TaskPreview.jsx
@@ -1,21 +1,24 @@
 import { DynamicCmp } from "../DynamicCmp"
 
 export function TaskPreview({ task }) {
-    const cmpsOrder = ['StatusPicker', 'MemberPicker', 'DatePicker', 'PriorityPicker']
+    // const cmpsOrder = ['StatusPicker', 'MemberPicker', 'DatePicker', 'PriorityPicker']
+    const cmpsOrder = ['StatusPicker']
     return (
         <section>
-            <h5>{task.txt}</h5>
             {cmpsOrder.map((cmp, idx) => {
                 return (
-                    <DynamicCmp
-                        cmp={cmp}
-                        key={idx}
-                        onUpdate={data => {
-                            console.log('Updating: ', cmp, 'with data:', data)
-                            // make a copy, update the task, create an action
-                            // Call action: updateTask(task, action)
-                        }}
-                    />
+                    <h5>{task.title}</h5>
+
+
+                    // <DynamicCmp
+                    //     cmp={cmp}
+                    //     key={idx}
+                    //     onUpdate={data => {
+                    //         console.log('Updating: ', cmp, 'with data:', data)
+                    //         // make a copy, update the task, create an action
+                    //         // Call action: updateTask(task, action)
+                    //     }}
+                    // />
                 )
             })}
         </section>
