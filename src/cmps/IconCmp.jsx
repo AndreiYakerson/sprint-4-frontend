3e5675eb02fb4c7881f2543cb2e79cefb3b2af94
Your Name <dan.weibren@gmail.com>
2025-09-30 11:22:55 +0300

    - Upgrade icon CMP in order to support positioning of floating container

--- src/cmps/IconCmp.jsx
+++ src/cmps/IconCmp.jsx
@@ -1,11 +1,28 @@
-export function IconCmp({ src, label = '', position = '', onClick }) {
+import React, { forwardRef } from 'react'
+
+export const IconCmp = forwardRef(({ src, label, position, onClick }, ref) => {
   return (
-    <span
-      className={`${label ? `hover-show ${position}` : 'icon'}`}
-      data-type={label}
-      onClick={(e) => onClick?.(e.currentTarget)}
-    >
-      <img className="icon" src={src} alt={`icon ${label}`} />
-    </span>
+    <img
+      ref={ref}
+      src={src}
+      alt={label || 'icon'}
+      className={`icon icon-${position || 'default'}`}
+      onClick={onClick}
+      style={{ cursor: 'pointer' }}
+    />
   )
-}
+})
+ 
+
+// Pest function 
+// export function IconCmp({ src, label = '', position = '', onClick }) {
+//   return (
+//     <span
+//       className={`${label ? `hover-show ${position}` : 'icon'}`}
+//       data-type={label}
+//       onClick={(e) => onClick?.(e.currentTarget)}
+//     >
+//       <img className="icon" src={src} alt={`icon ${label}`} />
+//     </span>
+//   )
+// }
