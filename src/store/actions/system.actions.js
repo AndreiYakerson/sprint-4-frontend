ec205e6e7a9e602b2c77daf540f4926915286b6c
Your Name <dan.weibren@gmail.com>
2025-10-04 12:07:57 +0300

    refactor(floating-container): manage floating open state in Redux
    - Add isFloatingOpen state and actions for floating container visibility
    - Use this state in FloatingContainerCmp and related UI for better control
    - Improve outside click handling and popup container logic

--- src/store/actions/system.actions.js
+++ src/store/actions/system.actions.js
@@ -1,12 +1,11 @@
 
-import { IS_POPUP_ON, SET_SIDE_BAR_OPEN } from '../reducers/system.reducer.js'
+import { IS_FLOATING_OPEN, IS_POPUP_ON, SET_SIDE_BAR_OPEN } from '../reducers/system.reducer.js'
 import { store } from '../store'
 
 
 export async function onSetIsSideBarOpen(value) {
     try {
         store.dispatch({ type: SET_SIDE_BAR_OPEN, value })
-        return console.log(`Side Bar is-open set to ${value}`)
     } catch (err) {
         console.log('Cannot Close Side Bar', err)
         throw err
@@ -16,9 +15,17 @@ export async function onSetIsSideBarOpen(value) {
 export async function onSetPopUpIsOpen(value) {
     try {
         store.dispatch({ type: IS_POPUP_ON, value })
-        return console.log(` PopUp is-open set to ${value}`)
     } catch (err) {
         console.log('Cannot Close PopUp Bar', err)
+        throw err
+    }
+}
+
+export async function onSetFloatingIsOpen(value) {
+    try {
+        store.dispatch({ type: IS_FLOATING_OPEN, value })
+    } catch (err) {
+        console.log('Cannot Close floating cmp', err)
         throw err
     }
 }
--- src/store/reducers/system.reducer.js
+++ src/store/reducers/system.reducer.js
@@ -2,12 +2,14 @@ export const LOADING_START = 'LOADING_START'
 export const LOADING_DONE = 'LOADING_DONE'
 
 export const IS_POPUP_ON = 'IS_POPUP_ON'
+export const IS_FLOATING_OPEN = 'IS_FLOATING_OPEN'
 export const SET_SIDE_BAR_OPEN = 'SET_SIDE_BAR_OPEN'
 
 const initialState = {
   isLoading: false,
   isSideBarOpen: true,
   isPopUpOpen: false,
+  isFloatingOpen: false,
 }
 
 export function systemReducer(state = initialState, action = {}) {
@@ -17,7 +19,10 @@ export function systemReducer(state = initialState, action = {}) {
     case LOADING_DONE:
       return { ...state, isLoading: false }
     case IS_POPUP_ON:
+      if (state.isFloatingOpen) state.isFloatingOpen = false
       return { ...state, isPopUpOpen: action.value }
+    case IS_FLOATING_OPEN:
+      return { ...state, isFloatingOpen: action.value }
     case SET_SIDE_BAR_OPEN:
       return { ...state, isSideBarOpen: action.value }
     default: return state
