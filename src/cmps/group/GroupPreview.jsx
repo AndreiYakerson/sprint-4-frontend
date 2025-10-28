adbc6724d13d494ffcfeb3bc7775d0d547ac7fbd
eliad <eliad.haim@gmail.com>
2025-10-28 15:56:31 +0200

    refactor groups to mobile

--- src/cmps/group/GroupPreview.jsx
+++ src/cmps/group/GroupPreview.jsx
@@ -212,6 +212,7 @@ export function GroupPreview({ group, groupsLength, managingType, TaskList,
 
     ///////////////////////////////////  Collapsed group ///////////////////////////////
 
+
     if (isCollapsed)
         return (
             <IndividualGroupCollapse
